c7eb9b2620673ebb14c9e69f440fcf25b33649f6
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 18:01:13 +0300

    Update ProtectedArray.cs

--- ProtectedArray.cs
+++ ProtectedArray.cs
@@ -1,17 +1,12 @@
 ﻿namespace _20_Lab8 {
 	class ProtectedArray {
-		// Напишіть програму, в якій є базовий клас
-		// із захищеним int масивом,
 		protected int[] array;
 
-		// індексатором (з int індексом),
-		// що дозволяє зчитувати та присвоювати значення елементів масиву,
 		public int this[int index] {
 			get => array[index];
 			set => array[index] = value;
 		}
 
-		// а також властивість, яка повертає у вигляді результату розмір масиву.
 		public int Length => array.Length;
 
 		public ProtectedArray(int[] array) => this.array = array;
